3d8ac2358fdff20318ebf637dffd5829ed07a231
Adam Lipok <lipokadam98@gmail.com>
2025-01-29 14:57:53 +0100

    Update edit course form

--- angular-signals-course-1-start/src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ angular-signals-course-1-start/src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -4,7 +4,7 @@ import {Course} from "../models/course.model";
 import {EditCourseDialogData} from "./edit-course-dialog.data.model";
 import {CoursesService} from "../services/courses.service";
 import {LoadingIndicatorComponent} from "../loading/loading.component";
-import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import {CourseCategoryComboboxComponent} from "../course-category-combobox/course-category-combobox.component";
 import {CourseCategory} from "../models/course-category.model";
 import {firstValueFrom} from "rxjs";
@@ -22,6 +22,25 @@ import {firstValueFrom} from "rxjs";
 })
 export class EditCourseDialogComponent {
   matDialogRef = inject(MatDialogRef);
+  data: EditCourseDialogData = inject(MAT_DIALOG_DATA);
+
+  fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    title: new FormControl("", [Validators.required]),
+    longDescription: new FormControl("", [Validators.required]),
+    category: new FormControl("", [Validators.required]),
+    iconUrl: new FormControl("", [Validators.required])
+  })
+
+  constructor() {
+    this.form.patchValue({
+      title: this.data?.title,
+      longDescription: this.data?.course?.longDescription,
+      category: this.data?.course?.category,
+      iconUrl: this.data?.course?.iconUrl,
+    })
+  }
 
   onClose() {
     this.matDialogRef.close();
